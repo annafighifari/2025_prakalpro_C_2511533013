eb9a18763c9f8b5c4ab8bd8fd13faffeb9b0018b
DashG <137711048+annafighifari@users.noreply.github.com>
2025-11-04 11:53:22 +0700

    Add files via upload
    praktikum alpro pekan 6_2511533013

---
+++ prakalpro_2025_c_2511533013/src/pekan6_2511533013/SentinelLoop_2511533013.java
@@ -0,0 +1,19 @@
+package pekan6_2511533013;
+
+import java.util.Scanner;
+public class SentinelLoop_2511533013 {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+		int sum = 0;
+		int number=12; // dummy value, apapun tapi 0
+		
+		while (number != 0) {
+			System.out.println("Masukkan angka (0 untuk keluar): ");
+			number = console.nextInt();
+			sum = sum + number;
+		}
+		System.out.println("totalnya adalah " + sum);
+	}
+
+}
