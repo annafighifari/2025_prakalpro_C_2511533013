f86fc08ac79b25bdeb42e57e110cfdcbad55eeee
DashG <137711048+annafighifari@users.noreply.github.com>
2025-11-11 12:06:18 +0700

    Add files via upload
    praktikum pekan 7 kelas C

---
+++ prakalpro_2025_c_2511533013/src/pekan7_2511533013/PanggilMahasiswa_2511533013.java
@@ -0,0 +1,12 @@
+package pekan7_2511533013;
+
+public class PanggilMahasiswa_2511533013 {
+    public static void main(String[] args) {
+        Mahasiswa_2511533013 a= new Mahasiswa_2511533013();
+        a.setNim(23532);
+        a.setNama("Rahmat");
+        System.out.println(a.getNim());
+        System.out.println(a.getNama());
+        a.cetak();
+    }
+}
